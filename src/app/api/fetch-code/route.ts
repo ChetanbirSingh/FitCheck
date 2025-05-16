95b0f1e8cbe76721e0ec091d215eb2c0ce2c11b7
Chetanbir Singh <chetanbirr@gmail.com>
2025-05-16 16:26:57 -0600

    feat: add API to fetch and structure decoded GitHub file content by filename

---
+++ src/app/api/fetch-code/route.ts
@@ -0,0 +1,37 @@
+export async function POST(req: Request) {
+  const { url, files } = await req.json();
+  const filesArray = files;
+  const githubUrl = url;
+  try {
+    const fetchedCode = await fetchCode(githubUrl, filesArray);
+    return Response.json(fetchedCode, { status: 200 });
+  } catch (error) {
+    return Response.json(
+      {
+        error: 'GitHub fetch failed. Check if the URL is correct and the repo is public.',
+      },
+      { status: 500 },
+    );
+  }
+}
+
+async function fetchCode(githubUrl: string, filesArray: string[]) {
+  const result: Record<string, { code: string }> = {};
+
+  for (const file of filesArray) {
+    const res = await fetch(`${githubUrl}/${file}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await res.json();
+
+    const decoded =
+      data.encoding === 'base64' ? Buffer.from(data.content, 'base64').toString('utf-8') : '';
+
+    result[file] = { code: decoded };
+  }
+
+  return result;
+}
