78d174226092fdea719e1831a41c55394e8b84ce
Chetanbir Singh <chetanbirr@gmail.com>
2025-05-27 21:27:01 -0600

    refactor: move ModesType and TechstackTypes with their allowed values to shared types/constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,21 @@
+export type TechstackTypes = 'html_css' | 'react' | 'next' | 'vue' | 'svelte' | 'solid' | 'angular';
+
+export const allowedStacks: TechstackTypes[] = [
+  'html_css',
+  'react',
+  'next',
+  'vue',
+  'svelte',
+  'solid',
+  'angular',
+];
+
+export type ModesType = 'mentor' | 'recruiter' | 'senior' | 'designer' | 'peer';
+
+export const allowedPersona: ModesType[] = [
+  'mentor',
+  'recruiter',
+  'senior',
+  'designer',
+  'peer',
+];
