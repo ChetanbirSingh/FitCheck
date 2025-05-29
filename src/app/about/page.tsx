f65ba9f8b4fbe96fef21557f8bc6a89d81335d1a
Chetanbir Singh <chetanbirr@gmail.com>
2025-05-29 14:03:39 -0600

    feat: add About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutSection from '@/components/about/AboutSection';
+import Footer from '@/components/layout/Footer';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <AboutSection />
+    </main>
+  );
+}
