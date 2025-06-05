a73b897029b63d7626976c4a1e6a91fb44dfafe3
Chetanbir Singh <chetanbirr@gmail.com>
2025-06-05 01:57:49 -0600

    refactor: update AboutSection to use new Paragraph component and reflect name origin

--- src/components/about/AboutSection.tsx
+++ src/components/about/AboutSection.tsx
@@ -1,24 +1,34 @@
 'use client';
 import { motion } from 'framer-motion';
-import PageHeader from '../pageHeader';
-import Paragraph from './Paragraph';
+import PageHeader from '../ui/pageHeader';
+import Paragraph from '../ui/Paragraph';
+
 export default function AboutSection() {
   return (
     <section className='max-w-3xl mx-auto md:pt-2 pt-10'>
       <PageHeader title='About Fitcheck' />
 
-      <Paragraph
-        text={`FitCheck was built to help developers get honest, useful feedback on their portfolios — without the guesswork.`}
-      />
+      <Paragraph>
+        FitCheck was built to help developers get honest, useful feedback on their portfolios —
+        without the guesswork.
+      </Paragraph>
 
-      <Paragraph
-        text={`Whether you're a beginner just getting started, a mid-level dev preparing for interviews, or
+      <Paragraph>
+        Whether you're a beginner just getting started, a mid-level dev preparing for interviews, or
         someone curious how others perceive your work — FitCheck gives you constructive, tailored
-        insights using AI personas like Mentors, Recruiters, Senior Devs, and more.`}
-      />
+        insights using AI personas like Mentors, Recruiters, Senior Devs, and more.
+      </Paragraph>
+
+      <div className='pt-10'>
+        <Paragraph>
+          The name “FitCheck” is a play on the phrase used in fashion — but here, it's your code
+          getting reviewed. Is it clean, clear, and job-ready? That's what FitCheck helps you figure
+          out.
+        </Paragraph>
+      </div>
 
       <motion.div
-        className='space-y-4'
+        className='space-y-4 pb-25'
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.6, duration: 0.6 }}
@@ -30,9 +40,6 @@ export default function AboutSection() {
           <li>Get feedback that;s actually helpful — not just “looks good”</li>
         </ul>
       </motion.div>
-      <div className='md:pt-25 pt-10 pb-25'>
-        <Paragraph text={`Not trying to pitch a company here. Just showing what I can do.`} />
-      </div>
     </section>
   );
 }
